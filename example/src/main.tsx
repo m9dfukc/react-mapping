6c4c19bb3a35a2266ba4d95e66709aab5c61502c
m9dfukc <andreas@invertednothing.com>
2020-02-10 12:30:32 +0100

    Extended Example

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Layer, Grid } from '../../';
-import KeyHandler, { KEYPRESS } from 'react-key-handler';
+import KeyHandler, { KEYPRESS, KEYUP, KEYDOWN } from 'react-key-handler';
 const YouTube = require('react-youtube').default;
 
 const getContainerStyle = (isEditMode) => ({
@@ -26,31 +26,76 @@ const opts = {
 
 export interface State {
   isEditMode: boolean;
+  save: boolean;
+  reset: boolean;
+  time: string;
 }
 
 class Main extends React.Component<{}, State> {
   state: State = {
-    isEditMode: true
+    isEditMode: false,
+    save: false,
+    reset: false,
+    time: (new Date).toLocaleTimeString()
   };
 
   toggleEdit = () => {
     this.setState({
       isEditMode: !this.state.isEditMode
     });
+  };
+  onSave = (t) => this.setState({save: (t && this.state.isEditMode)});
+  onReset = (t) => this.setState({reset: (t && this.state.isEditMode)});
+
+  counter = () => {
+    setTimeout(() => {
+      this.setState({time: (new Date).toLocaleTimeString()})
+      this.counter();
+    }, 1000);
+  };
+
+  componentDidMount() {
+    this.counter();
   }
 
   render() {
-    const { isEditMode } = this.state;
-
+    const { isEditMode, save, reset, time } = this.state;
     return (
     <div style={getContainerStyle(isEditMode)}>
       <KeyHandler keyEventName={KEYPRESS} keyValue="t" onKeyHandle={this.toggleEdit} />
+      <KeyHandler keyEventName={KEYDOWN} keyValue="s" onKeyHandle={() => this.onSave(true)} />
+      <KeyHandler keyEventName={KEYUP} keyValue="s" onKeyHandle={() => this.onSave(false)} />
+      <KeyHandler keyEventName={KEYDOWN} keyValue="r" onKeyHandle={() => this.onReset(true)} />
+      <KeyHandler keyEventName={KEYUP} keyValue="r" onKeyHandle={() => this.onReset(false)} />
+
       {isEditMode && <Grid/>}
-      <Layer style={opts} x={100} y={40} isEditMode={isEditMode}>
+      <Layer
+        label="layer-1"
+        style={opts}
+        x={100}
+        y={40}
+        isEditMode={isEditMode}
+        save={save}
+        reload={reset}>
         <YouTube videoId="M6khNkdbt1w" opts={opts}/>
       </Layer>
-      <Layer style={opts} x={150} y={300} isEditMode={isEditMode}>
-        <YouTube videoId="LXgTp40Y3zo" opts={opts}/>
+      <Layer
+        label="layer-2"
+        x={400}
+        y={100}
+        isEditMode={isEditMode}
+        save={save}
+        reload={reset}
+        style={{
+          width: 200,
+          height: 200,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          color: 'white',
+          border: '1px solid white'
+        }}>
+        {time}
       </Layer>
       <div style={instruction}>Press "t" to switch between edit / view mode</div>
     </div>
